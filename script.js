f25220ebb7ab62878a79703cd2d890b0f954adaa
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-07-08 11:51:39 +0530

    add a button to prompt nos. of squares per line

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-const container = document.querySelector('#container');
+var gridBtn = document.getElementById("grid-btn");
 
-const GRID = 32;
-for(let i = 0; i < GRID; ++i){
-    const inner_div = document.createElement('div')
-    for(let j = 0; j < GRID; ++j){
-        const inner_most_div = document.createElement('div')
-        inner_most_div.classList.add('pixel')
-        inner_most_div.setAttribute('id', `${i}-${j}`)
-        inner_div.appendChild(inner_most_div)
-    }
-    container.appendChild(inner_div)
+
+
+
+function gridCreate(){
+    gridVal = parseInt(prompt("Nos. of squares per line:"))
+    makeGrid(gridVal);
+    return 1
 }
+let flagGrid = 0;
+gridBtn.addEventListener("click", function(){
+    //let gridVal = 0;
+    if (flagGrid == 1) {
+        document.getElementById('container').innerHTML = ''
+        gridCreate()
+    }
+    else {
+        flagGrid = gridCreate()
+    }
+});
 
 
-    const pxl = document.querySelectorAll('.pixel')
 
-    pxl.forEach((px) => {
-        px.addEventListener('mouseover', ()=>{
-            let currentPxl = document.getElementById(px.id);
-            currentPxl.setAttribute('style', 'background: black;');
+
+function makeGrid(gridVal){
+    const container = document.querySelector('#container');
+
+    let grid = gridVal;
+    for(let i = 0; i < grid; ++i){
+        const inner_div = document.createElement('div')
+        for(let j = 0; j < grid; ++j){
+            const inner_most_div = document.createElement('div')
+            inner_most_div.classList.add('pixel')
+            inner_most_div.setAttribute('id', `${i}-${j}`)
+            inner_div.appendChild(inner_most_div)
+        }
+        container.appendChild(inner_div)
+    }
+
+
+        const pxl = document.querySelectorAll('.pixel')
+
+        pxl.forEach((px) => {
+            px.addEventListener('mouseover', ()=>{
+                let currentPxl = document.getElementById(px.id);
+                currentPxl.setAttribute('style', 'background: black;');
+            })
         })
-    })
-
+}
 
 
     
